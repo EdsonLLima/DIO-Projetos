8f200e381d53502bd577dfdc17913fb9b1da8284
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2021-11-08 13:58:01 -0300

    criação da logina do jogo| implementando funções for para capturar as cores

--- genius/assets/scripts/genius.js
+++ genius/assets/scripts/genius.js
@@ -19,6 +19,17 @@ let shuffleOrder = ()=>{
 
   for(let i in order){
     let elementColor = createColorElement(order[i]);
-    // Parte 3
+    lightColor(elementColor, Number(i) + 1);
   }
+}
+
+let lightColor = (element, number) => {
+  number = number * 500;
+  setTimeout(()=>{
+    element.classList.add('selected');
+  }, number+250);
+
+  setTimeout(()=>{
+    element.classList.remove('selected');
+  });
 }
