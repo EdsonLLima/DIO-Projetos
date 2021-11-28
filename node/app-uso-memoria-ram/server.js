a1b1961febd70546ab708800f5b016a52a25da26
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2021-11-28 14:58:38 -0300

    criado app em node de gestao de memoria ram

---
+++ node/app-uso-memoria-ram/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const host = 'http://localhost';
+const port = 3000;
+const stats = require('./pcRamUsage.js');
+
+http.createServer((req, res) => {
+	let url = req.url;
+
+	if(url === "/stats"){
+		res.end(JSON.stringify(stats, null, 2));
+	}else {
+		res.end("<h1>Seja bem-vindo</h1>")
+	}
+
+}).listen(port, () => console.log(`Server is running in ${host}:${port}`));
