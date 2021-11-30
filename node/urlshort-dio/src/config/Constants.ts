caed63d15ecc32a244fd10d110857efd3fce9dec
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2021-11-29 22:08:38 -0300

    criado app urlshort

---
+++ node/urlshort-dio/src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://dbadmin:dbadmin@urlnode.l2174.mongodb.net/dbadmin2021?retryWrites=true&w=majority',
+}
