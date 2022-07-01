74a5c0631099421eacec5e593c6c9a5b5b13bd4a
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2022-07-01 15:33:11 -0300

    criado component Card e Button

--- reactjs/component-react-rev/src/components/Card/index.js
+++ reactjs/component-react-rev/src/components/Card/index.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import Button from "../Button";
+
 const Card = () => {
+  const [valor, setValor] = useState(0);
+
+  function Adicionar() {
+    setValor(valor + 1);
+  }
+
+  function Remover() {
+    setValor(valor - 1);
+  }
+
   return (
-    <div class="card">
-      <h5 class="card-header">Featured</h5>
-      <div class="card-body">
-        <p class="card-text">With supporting text bel.</p>
+    <div className="card">
+      <h5 className="card-header">Featured</h5>
+      <div className="card-body">
+        <Button className="btn btn-success" onClick={Adicionar}>
+          Adicionar
+        </Button>
+        <Button className="btn btn-danger" onClick={Remover}>
+          Remover
+        </Button>
+        <p>{valor}</p>
       </div>
     </div>
   );
